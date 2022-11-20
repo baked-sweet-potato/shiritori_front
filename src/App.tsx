862c48f229f55466f3f5287a0047291c03c63eea
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2022-11-20 16:10:46 +0900

    add: チャット画面

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput} from '@chatscope/chat-ui-kit-react'
 import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 
 function App(): JSX.Element {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ position: 'relative', height: '800px', width: '1280px' }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: 'Hello my friend',
+                direction: 'incoming',
+                sender: 'Joe',
+                sentTime: 'just now',
+                position: 'single'
+              }}
+            >
+              <Message.Footer sender="Emily" sentTime="just now" />
+            </Message>
+            <Message
+              model={{
+                message: 'Hello my friend',
+                direction: 'incoming',
+                sender: 'Joe',
+                sentTime: 'just now',
+                position: 'single'
+              }}
+            >
+              <Message.Footer sender="Emily" sentTime="just now" />
+            </Message>
+          </MessageList>
+          <MessageInput
+            style={{ textAlign: 'left' }}
+            placeholder="Type message here"
+            attachButton={false}
+          />
+        </ChatContainer>
+      </MainContainer>
     </div>
   )
 }
