432056b58f39fcba2b642e05fa9c0ca031e64887
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2022-12-10 19:05:44 +0900

    update: レイアウト

--- src/App.tsx
+++ src/App.tsx
@@ -21,22 +21,16 @@ import { ExampleChatService } from '@chatscope/use-chat/dist/examples'
 import { Chat } from './components/Chat'
 import { nanoid } from 'nanoid'
 import { Col, Container, Row } from 'react-bootstrap'
-import {
-  akaneModel,
-  eliotModel,
-  emilyModel,
-  joeModel,
-  users
-} from './data/data'
+import { akaneModel, users } from './data/data'
 import { AutoDraft } from '@chatscope/use-chat/dist/enums/AutoDraft'
 
 // sendMessage and addMessage methods can automagically generate id for messages and groups
 // This allows you to omit doing this manually, but you need to provide a message generator
 // The message id generator is a function that receives message and returns id for this message
 // The group id generator is a function that returns string
-const messageIdGenerator = (message: ChatMessage<MessageContentType>) : string =>
+const messageIdGenerator = (message: ChatMessage<MessageContentType>): string =>
   nanoid()
-const groupIdGenerator = () : string => nanoid()
+const groupIdGenerator = (): string => nanoid()
 
 const akaneStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator })
 const eliotStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator })
@@ -44,7 +38,10 @@ const emilyStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator })
 const joeStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator })
 
 // Create serviceFactory
-const serviceFactory = (storage: IStorage, updateState: UpdateState): ExampleChatService => {
+const serviceFactory = (
+  storage: IStorage,
+  updateState: UpdateState
+): ExampleChatService => {
   return new ExampleChatService(storage, updateState)
 }
 
@@ -56,39 +53,6 @@ const akane = new User({
   username: akaneModel.name,
   email: '',
   avatar: akaneModel.avatar,
-  bio: ''
-})
-
-const emily = new User({
-  id: emilyModel.name,
-  presence: new Presence({ status: UserStatus.Available, description: '' }),
-  firstName: '',
-  lastName: '',
-  username: emilyModel.name,
-  email: '',
-  avatar: emilyModel.avatar,
-  bio: ''
-})
-
-const eliot = new User({
-  id: eliotModel.name,
-  presence: new Presence({ status: UserStatus.Available, description: '' }),
-  firstName: '',
-  lastName: '',
-  username: eliotModel.name,
-  email: '',
-  avatar: eliotModel.avatar,
-  bio: ''
-})
-
-const joe = new User({
-  id: joeModel.name,
-  presence: new Presence({ status: UserStatus.Available, description: '' }),
-  firstName: '',
-  lastName: '',
-  username: joeModel.name,
-  email: '',
-  avatar: joeModel.avatar,
   bio: ''
 })
 
@@ -166,14 +130,14 @@ chats.forEach((c) => {
   })
 })
 
-export const App = () : JSX.Element => {
+export const App = (): JSX.Element => {
   return (
     <div className="h-100 d-flex flex-column overflow-hidden">
       <Container
         fluid
         className="p-4 flex-grow-1 position-relative overflow-hidden"
       >
-        <Row className="h-50 pb-2 flex-nowrap">
+        <Row style={{ width: '1200px', height: '650px' }}>
           <Col>
             <ChatProvider
               serviceFactory={serviceFactory}
@@ -186,50 +150,6 @@ export const App = () : JSX.Element => {
               }}
             >
               <Chat user={akane} />
-            </ChatProvider>
-          </Col>
-          <Col>
-            <ChatProvider
-              serviceFactory={serviceFactory}
-              storage={eliotStorage}
-              config={{
-                typingThrottleTime: 250,
-                typingDebounceTime: 900,
-                debounceTyping: true,
-                autoDraft: AutoDraft.Save | AutoDraft.Restore
-              }}
-            >
-              <Chat user={eliot} />
-            </ChatProvider>
-          </Col>
-        </Row>
-        <Row className="h-50 pt-2 flex-nowrap">
-          <Col>
-            <ChatProvider
-              serviceFactory={serviceFactory}
-              storage={emilyStorage}
-              config={{
-                typingThrottleTime: 250,
-                typingDebounceTime: 900,
-                debounceTyping: true,
-                autoDraft: AutoDraft.Save | AutoDraft.Restore
-              }}
-            >
-              <Chat user={emily} />
-            </ChatProvider>
-          </Col>
-          <Col>
-            <ChatProvider
-              serviceFactory={serviceFactory}
-              storage={joeStorage}
-              config={{
-                typingThrottleTime: 250,
-                typingDebounceTime: 900,
-                debounceTyping: true,
-                autoDraft: AutoDraft.Save | AutoDraft.Restore
-              }}
-            >
-              <Chat user={joe} />
             </ChatProvider>
           </Col>
         </Row>
